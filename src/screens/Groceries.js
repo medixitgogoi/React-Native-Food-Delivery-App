c08dbdaf0941cccaa9a59b773d32eb5b778c6a9a
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-08-03 14:27:30 +0530

    updated

--- src/screens/Groceries.js
+++ src/screens/Groceries.js
@@ -1,18 +1,56 @@
-import { StatusBar, StyleSheet, View } from 'react-native';
-import { background } from '../utils/colors';
+import { StatusBar, StyleSheet, TouchableOpacity, View, Text, TextInput } from 'react-native';
+import { background, backIconColor, darkGreen, lightGreen } from '../utils/colors';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { responsiveFontSize } from 'react-native-responsive-dimensions';
+import Icon from 'react-native-vector-icons/dist/MaterialIcons';
+import Icon2 from 'react-native-vector-icons/dist/Octicons';
+import Icon3 from 'react-native-vector-icons/dist/Ionicons';
+import Icon4 from 'react-native-vector-icons/dist/MaterialCommunityIcons';
+import { useState } from 'react';
 
 const Groceries = () => {
+
+    const navigation = useNavigation();
+
+    const [search, setSearch] = useState('');
+    const [isSearchFocused, setIsSearchFocused] = useState(false);
+
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: background, paddingBottom: 10 }}>
             <StatusBar
                 animated={true}
-                backgroundColor={background}
+                backgroundColor={darkGreen}
                 barStyle="dark-content"
             />
 
             {/* header */}
-            <View>
+            <View style={{ flexDirection: "column", backgroundColor: darkGreen, elevation: 1, paddingHorizontal: 10, paddingTop: 5, paddingBottom: 8 }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: "100%", }}>
+                    <View style={{ paddingVertical: 8, flexDirection: "row", alignItems: "center", gap: 6 }}>
+                        <TouchableOpacity style={{ width: 30, height: 30, backgroundColor: '#fff', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', borderRadius: 8, elevation: 3 }} onPress={() => navigation.goBack()}>
+                            <Icon name="keyboard-arrow-left" size={23} color={backIconColor} />
+                        </TouchableOpacity>
+                        <Text style={{ color: '#fff', fontWeight: "500", fontSize: responsiveFontSize(2.5), textAlign: 'center', width: '83%', textTransform: 'uppercase' }}>Groceries</Text>
+                    </View>
+                </View>
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <View style={{ width: '85%', borderColor: isSearchFocused ? backIconColor : '#F9FAFD', borderWidth: 1, flexDirection: 'row', alignItems: 'center', backgroundColor: '#fff', borderRadius: 11, paddingHorizontal: 8, elevation: 1 }}>
+                        <Icon2 name="search" size={16} color={'#687889'} style={{ padding: 5 }} />
+                        <TextInput
+                            style={{ height: 38, color: '#000', fontWeight: '500', width: '87%' }}
+                            placeholder="Search Grocery"
+                            placeholderTextColor="#a0abb7"
+                            onChangeText={setSearch}
+                            value={search}
+                            onFocus={() => setIsSearchFocused(true)}
+                            onBlur={() => setIsSearchFocused(false)}
+                        />
+                    </View>
+                    <TouchableOpacity style={{ backgroundColor: '#fff', borderRadius: 8, width: 38, height: 38, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                        <Icon4 name="filter-menu-outline" size={20} color={backIconColor} />
+                    </TouchableOpacity>
+                </View>
 
             </View>
 
