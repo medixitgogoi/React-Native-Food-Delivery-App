ebe0d2df28e174b68e0e8a4bd77364fb30306ec2
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-08-05 11:56:23 +0530

    groceries page updated

--- src/screens/Groceries.js
+++ src/screens/Groceries.js
@@ -1,13 +1,13 @@
-import { StatusBar, StyleSheet, TouchableOpacity, View, Text, TextInput } from 'react-native';
+import { StatusBar, StyleSheet, TouchableOpacity, View, Text, TextInput, Image } from 'react-native';
 import { background, backIconColor, darkGreen, lightGreen } from '../utils/colors';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { responsiveFontSize } from 'react-native-responsive-dimensions';
 import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 import Icon2 from 'react-native-vector-icons/dist/Octicons';
-import Icon3 from 'react-native-vector-icons/dist/Ionicons';
 import Icon4 from 'react-native-vector-icons/dist/MaterialCommunityIcons';
 import { useState } from 'react';
+import { groceries } from '../utils/groceries';
 
 const Groceries = () => {
 
@@ -53,6 +53,35 @@ const Groceries = () => {
                 </View>
 
             </View>
+
+            {groceries.map(item => (
+                <View key={item?.id} style={{ width: 150, backgroundColor: 'white', borderTopLeftRadius: 14, borderTopRightRadius: 14, borderBottomLeftRadius: 14, borderBottomRightRadius: 20, overflow: 'hidden', margin: 10, elevation: 5, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.2, shadowRadius: 2 }}>
+
+                    <View style={{ backgroundColor: lightGreen, borderRadius: 12, margin: 3 }}>
+                        <TouchableOpacity style={{ backgroundColor: '#c6e6c3', borderRadius: 50, position: 'absolute', top: 5, right: 5, width: 30, height: 30, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                            <Icon name="favorite-border" size={20} color="green" />
+                        </TouchableOpacity>
+                        <View style={{ padding: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                            <Image source={require('../assets/orange.png')} style={{ width: '100%', height: 100, resizeMode: 'contain' }} />
+                        </View>
+                    </View>
+
+                    <View>
+                        <Text style={{ fontSize: responsiveFontSize(2), fontWeight: 'bold', marginTop: 10, color: '#000' }}>{item.name}</Text>
+                        <View style={{ flexDirection: 'row', marginVertical: 5 }}>
+                            {[...Array(4)].map((_, index) => (
+                                <Icon key={index} name="star" size={16} color="#FFA41C" />
+                            ))}
+                            <Icon name="star-border" size={16} color="#FFA41C" />
+                        </View>
+                        <Text style={{ fontSize: 16, color: 'green', fontWeight: 'bold' }}>₹{item.price}</Text>
+                        <TouchableOpacity style={{ backgroundColor: 'green', borderTopLeftRadius: 10, width: 35, height: 35, justifyContent: 'center', alignItems: 'center', position: 'absolute', bottom: 0, right: 0 }}>
+                            <Icon name="add" size={20} color="white" />
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+            ))}
 
         </SafeAreaView>
     )
