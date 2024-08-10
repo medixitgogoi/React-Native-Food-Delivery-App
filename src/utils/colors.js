489e5edc50048391a57b39fd9c4694fc47e3ac2d
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-08-10 12:45:49 +0530

    updated

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,5 +1,5 @@
 export const lightGreen = '#EDF7EC';
 export const darkGreen = '#5EC467';
-export const background = '#F9FAFD';
+export const background = '#F6FBF7';
 export const backIconColor = '#286d2e';
 export const offWhite = "#b5b9bf";
