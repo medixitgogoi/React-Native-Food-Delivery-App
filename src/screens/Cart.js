fdf3bf267e82fda7770541b95cae569fb591cae6
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-08-13 18:25:10 +0530

    updated

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -6,6 +6,7 @@ import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 import Icon2 from 'react-native-vector-icons/dist/MaterialCommunityIcons';
 import { groceries } from '../utils/groceries';
 import Icon3 from 'react-native-vector-icons/dist/FontAwesome6';
+import Icon4 from 'react-native-vector-icons/dist/AntDesign';
 import Icon5 from 'react-native-vector-icons/dist/AntDesign';
 
 const Cart = () => {
@@ -16,7 +17,7 @@ const Cart = () => {
         <View>
             <Text style={{ color: '#000' }}>{item.name}</Text>
         </View>
-    }
+    };
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: background, paddingBottom: 10 }}>
@@ -63,7 +64,7 @@ const Cart = () => {
                     </View>
                 </View>
 
-                <View style={{ paddingHorizontal: 4, backgroundColor: '#fff', borderRadius: 12, elevation: 1, flexDirection: 'row', alignItems: 'center', overflow: 'hidden', height: 100, marginTop: 10 }}>
+                <View style={{ paddingHorizontal: 4, backgroundColor: '#fff', borderRadius: 12, elevation: 1, flexDirection: 'row', alignItems: 'center', overflow: 'hidden', height: 100, marginTop: 8 }}>
                     <View style={{ padding: 10, flexDirection: 'row', borderRadius: 10, alignItems: 'center', justifyContent: 'center', flex: 1, backgroundColor: '#e4f4ea', height: 90 }}>
                         <Image source={require('../assets/rice.png')} style={{ width: '100%', height: '100%', resizeMode: 'contain' }} />
                     </View>
@@ -91,7 +92,7 @@ const Cart = () => {
                     </View>
                 </View>
 
-                <View style={{ paddingHorizontal: 4, backgroundColor: '#fff', borderRadius: 12, elevation: 1, flexDirection: 'row', alignItems: 'center', overflow: 'hidden', height: 100, marginTop: 10 }}>
+                <View style={{ paddingHorizontal: 4, backgroundColor: '#fff', borderRadius: 12, elevation: 1, flexDirection: 'row', alignItems: 'center', overflow: 'hidden', height: 100, marginTop: 8 }}>
                     <View style={{ padding: 10, flexDirection: 'row', borderRadius: 10, alignItems: 'center', justifyContent: 'center', flex: 1, backgroundColor: '#e4f4ea', height: 90 }}>
                         <Image source={require('../assets/cake.png')} style={{ width: '100%', height: '100%', resizeMode: 'contain' }} />
                     </View>
@@ -119,8 +120,28 @@ const Cart = () => {
                     </View>
                 </View>
 
-                <View style={{ backgroundColor: '#fff' }}>
+                <View style={{ backgroundColor: '#fff', padding: 8, marginTop: 20, elevation: 1, borderRadius: 12, paddingTop: 10, paddingHorizontal: 12 }}>
+                    <Text style={{ textAlign: 'center', fontSize: responsiveFontSize(2.5), fontWeight: '700', textTransform: 'uppercase', color: '#000', marginBottom: 10 }}>Cart Total</Text>
 
+                    <View style={{ flexDirection: 'column', justifyContent: 'center', width: '100%', marginTop: 5, gap: 4 }}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'space-between' }}>
+                            <Text style={{ color: '#838a94', fontWeight: '500', fontSize: responsiveFontSize(2.1) }}>Sub Total</Text>
+                            <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.3) }}>₹590.00</Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'space-between' }}>
+                            <Text style={{ color: '#838a94', fontWeight: '500', fontSize: responsiveFontSize(2.1) }}>Delivery Charges</Text>
+                            <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.3) }}>₹50.00</Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'space-between' }}>
+                            <Text style={{ color: '#838a94', fontWeight: '500', fontSize: responsiveFontSize(2.1) }}>Discount</Text>
+                            <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.3) }}>₹120.00</Text>
+                        </View>
+                        <View style={{ borderStyle: 'dashed', borderWidth: 0.6, borderColor: offWhite, marginVertical: 5 }}></View>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'space-between', paddingVertical: 5 }}>
+                            <Text style={{ color: '#60666f', fontWeight: '500', fontSize: responsiveFontSize(2.1) }}>Final Total</Text>
+                            <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.3) }}>₹410.00</Text>
+                        </View>
+                    </View>
                 </View>
 
             </View>
@@ -133,6 +154,19 @@ const Cart = () => {
                 contentContainerStyle={{ paddingHorizontal: 10, paddingBottom: 90, paddingTop: 4 }}
                 // columnWrapperStyle={{ justifyContent: 'space-between' }}
             /> */}
+
+            <View style={{ position: 'absolute', bottom: 0, backgroundColor: lightGreen, width: '100%', height: 65, elevation: 2, flexDirection: 'row', alignItems: 'center' }}>
+                <View style={{ flex: 1.2, flexDirection: 'column', paddingHorizontal: 20, gap: 1 }}>
+                    <Text style={{ color: '#838a94', fontWeight: '500', fontSize: responsiveFontSize(2) }}>Total Price</Text>
+                    <Text style={{ color: '#000', fontWeight: '800', fontSize: responsiveFontSize(2.8) }}>₹1300</Text>
+                </View>
+                <View style={{ flex: 3, height: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', paddingRight: 10 }}>
+                    <TouchableOpacity style={{ backgroundColor: backIconColor, borderRadius: 50, width: '90%', padding: 10, height: 45, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: 6}}>
+                        <Text style={{ color: '#fff', fontWeight: '700', textAlign: 'center', fontSize: responsiveFontSize(2.3) }}>Checkout</Text>
+                        <Icon4 name="arrowright" size={23} color={'#fff'} />
+                    </TouchableOpacity>
+                </View>
+            </View>
 
         </SafeAreaView>
     )
