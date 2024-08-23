622ca830a0055b1ca917b73113fc8ffaf9ff5fe1
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-08-23 12:15:08 +0530

    updated

--- src/auth/OtpVerification.js
+++ src/auth/OtpVerification.js
@@ -4,7 +4,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import { responsiveFontSize } from 'react-native-responsive-dimensions';
 import Icon4 from 'react-native-vector-icons/dist/AntDesign';
 import { useNavigation } from '@react-navigation/native';
-import { darkGreen } from '../utils/colors';
+import { darkGreen, offWhite } from '../utils/colors';
 import { useState } from 'react';
 
 const OtpVerification = ({ route }) => {
@@ -14,7 +14,7 @@ const OtpVerification = ({ route }) => {
 
     const navigation = useNavigation();
 
-    const [mobileNumber, setMobileNumber] = useState('');
+    const [mobileNumber, setMobileNumber] = useState('443435353535');
 
     const otpHandler = () => {
 
@@ -32,7 +32,6 @@ const OtpVerification = ({ route }) => {
             }
         }
     };
-
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
@@ -64,38 +63,52 @@ const OtpVerification = ({ route }) => {
                         <Image source={require('../assets/mobile.png')} style={{ width: 200, height: 200, resizeMode: 'contain' }} />
                     </View>
 
-                    <View style={{ flexDirection: 'column', gap: 1 }}>
-                        <Text style={{ color: '#000', fontSize: responsiveFontSize(2.5), fontWeight: '700', textAlign: 'center' }}>Enter Your Mobile Number</Text>
-                        <Text style={{ color: '#444444', fontSize: responsiveFontSize(1.9), fontWeight: '500', textAlign: 'center' }}>We'll send a confirmation code to verify it's really you</Text>
-                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginTop: 20 }}>
-                            <View style={{ height: 40, flex: 0.165, borderColor: '#4d4d4d', borderWidth: 1, borderRadius: 8, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#d9eed7' }}>
-                                <Text style={{ color: '#000', fontWeight: '700', fontSize: responsiveFontSize(2) }}>+91</Text>
+                    {/* <View>
+                        <View style={{ flexDirection: 'column', gap: 1 }}>
+                            <Text style={{ color: '#000', fontSize: responsiveFontSize(2.5), fontWeight: '700', textAlign: 'center' }}>Enter Your Mobile Number</Text>
+                            <Text style={{ color: '#444444', fontSize: responsiveFontSize(1.9), fontWeight: '500', textAlign: 'center' }}>We'll send a confirmation code to verify it's really you</Text>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginTop: 20 }}>
+                                <View style={{ height: 40, flex: 0.165, borderColor: '#4d4d4d', borderWidth: 1, borderRadius: 8, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#d9eed7' }}>
+                                    <Text style={{ color: '#000', fontWeight: '700', fontSize: responsiveFontSize(2) }}>+91</Text>
+                                </View>
+                                <View style={{ flex: 0.8 }}>
+                                    <TextInput
+                                        style={{ height: 40, borderColor: '#4d4d4d', fontWeight: "500", borderWidth: 1, borderRadius: 8, paddingHorizontal: 15, fontSize: responsiveFontSize(2), color: '#000', backgroundColor: '#fff' }}
+                                        placeholder="Enter Phone Number"
+                                        keyboardType="numeric"
+                                        maxLength={10}
+                                        value={mobileNumber}
+                                        onChangeText={setMobileNumber}
+                                        placeholderTextColor="#afb8c2"
+                                    />
+                                </View>
                             </View>
-                            <View style={{ flex: 0.8 }}>
-                                <TextInput
-                                    style={{ height: 40, borderColor: '#4d4d4d', fontWeight: "500", borderWidth: 1, borderRadius: 8, paddingHorizontal: 15, fontSize: responsiveFontSize(2), color: '#000', backgroundColor: '#fff' }}
-                                    placeholder="Enter Phone Number"
-                                    keyboardType="numeric"
-                                    maxLength={10}
-                                    value={mobileNumber}
-                                    onChangeText={setMobileNumber}
-                                    placeholderTextColor="#afb8c2"
-                                />
-                            </View>
+                        </View>
+
+                        <LinearGradient
+                            colors={[darkGreen, '#3a9f43']}
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 1, y: 0 }}
+                            style={{ borderRadius: 12, paddingHorizontal: 24, elevation: 2, marginTop: 30 }}
+                        >
+                            <TouchableOpacity onPress={handleOTPVerificationSuccess} style={{ gap: 5, width: '100%', height: 47, borderRadius: 12, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                                <Text style={{ color: '#fff', fontSize: responsiveFontSize(2.5), fontWeight: '600' }}>Send OTP</Text>
+                                <Icon4 name="arrowright" size={23} color='#fff' />
+                            </TouchableOpacity>
+                        </LinearGradient>
+                    </View> */}
+
+                    <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+                        <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+                            <Text style={{ color: '#000', fontWeight: '500', fontSize: responsiveFontSize(2) }}>We have sent a verification code to</Text>
+                            <Text style={{ color: '#000', fontWeight: '800', fontSize: responsiveFontSize(2) }}>+91 {mobileNumber}</Text>
+                        </View>
+
+                        <View style={{ flexDirection: 'column', alignItems: 'center', marginTop: 8 }}>
+                            <Text style={{ color: offWhite, fontWeight: '500', fontSize: responsiveFontSize(1.9) }}>Enter Your OTP Code Below</Text>
                         </View>
                     </View>
 
-                    <LinearGradient
-                        colors={[darkGreen, '#3a9f43']}
-                        start={{ x: 0, y: 0 }}
-                        end={{ x: 1, y: 0 }}
-                        style={{ borderRadius: 12, paddingHorizontal: 24, elevation: 2, marginTop: 30 }}
-                    >
-                        <TouchableOpacity onPress={handleOTPVerificationSuccess} style={{ gap: 5, width: '100%', height: 47, borderRadius: 12, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                            <Text style={{ color: '#fff', fontSize: responsiveFontSize(2.5), fontWeight: '600' }}>Send OTP</Text>
-                            <Icon4 name="arrowright" size={23} color='#fff' />
-                        </TouchableOpacity>
-                    </LinearGradient>
                 </View>
             </LinearGradient>
         </SafeAreaView>
