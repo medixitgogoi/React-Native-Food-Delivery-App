c068ee967f89f8ec31dfce759d6ac57f3d0574fb
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-08-27 17:07:25 +0530

    updated

--- src/screens/AddNewAddress.js
+++ src/screens/AddNewAddress.js
@@ -4,10 +4,11 @@ import { responsiveFontSize } from 'react-native-responsive-dimensions';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 import Icon2 from 'react-native-vector-icons/dist/MaterialCommunityIcons';
-import Icon3 from 'react-native-vector-icons/dist/Feather';
-import Icon4 from 'react-native-vector-icons/dist/Ionicons';
+import Icon3 from 'react-native-vector-icons/dist/FontAwesome5';
+import Icon4 from 'react-native-vector-icons/dist/Feather';
 import { useEffect, useState } from 'react';
 import { data } from '../utils/address';
+import LinearGradient from 'react-native-linear-gradient';
 
 const AddNewAddress = () => {
 
@@ -38,13 +39,14 @@ const AddNewAddress = () => {
 
             {/* Content */}
             <View style={{ padding: 10 }}>
+                {/* Unexpanded */}
                 <TouchableOpacity style={{ backgroundColor: '#fff', paddingHorizontal: 13, paddingVertical: 12, flexDirection: 'column', elevation: 1, borderRadius: 12, gap: 6 }}>
                     <View>
                         <Text style={{ color: '#9297a0', fontWeight: '500', fontSize: responsiveFontSize(1.9) }}>Receiver details for this address</Text>
                     </View>
                     <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', }}>
                         <View style={{ flexDirection: 'row', alignItems: 'center', gap: 3 }}>
-                            <Icon3 name="phone-call" size={15} color={'#000'} />
+                            <Icon4 name="phone-call" size={15} color={'#000'} />
                             <Text style={{ textTransform: 'uppercase', fontSize: responsiveFontSize(1.9), color: '#000', fontWeight: '500', marginLeft: 5 }}>Dixit Gogoi,</Text>
                             <Text style={{ textTransform: 'uppercase', fontSize: responsiveFontSize(1.9), color: '#000' }}>6000578700</Text>
                         </View>
@@ -54,78 +56,71 @@ const AddNewAddress = () => {
                     </View>
                 </TouchableOpacity>
 
-                <View style={{ backgroundColor: '#fff', marginTop: 10 }}>
+                {/* Expanded */}
+                <View style={{ marginTop: 10, backgroundColor: '#fff', paddingHorizontal: 13, paddingVertical: 12, flexDirection: 'column', elevation: 1, borderRadius: 12, gap: 6 }}>
                     {/* Receiver's Name */}
-                    <View style={{ marginBottom: 16 }}>
-                        <Text style={{ fontSize: 14, marginBottom: 8, color: '#555' }}>Receiver’s name</Text>
-                        <View style={{ flexDirection: 'row', alignItems: 'center', borderWidth: 1, borderColor: '#e0e0e0', borderRadius: 8, padding: 10, backgroundColor: '#fff' }}>
-                            <TextInput
-                                value={name}
-                                onChangeText={setName}
-                                placeholder="Enter receiver's name"
-                                style={{ flex: 1, fontSize: 16, color: '#000' }}
-                            />
-                            <TouchableOpacity onPress={() => setName('')}>
-                                <Icon name="close" size={20} color="#999" />
-                            </TouchableOpacity>
-                        </View>
+                    <View style={{ marginBottom: 10 }}>
+                        <Text style={{ color: '#9297a0', fontWeight: '500', fontSize: responsiveFontSize(1.9), marginBottom: 8 }}>Receiver’s name</Text>
+                        <TextInput
+                            style={{ height: 40, borderColor: offWhite, fontWeight: "500", borderWidth: 1.2, borderRadius: 8, paddingHorizontal: 15, fontSize: responsiveFontSize(2), color: '#000', backgroundColor: '#fff' }}
+                            placeholder="Enter Name"
+                            value={name}
+                            onChangeText={setName}
+                            placeholderTextColor={'#c8cacf'}
+                        />
                     </View>
 
                     {/* Receiver's Contact */}
-                    <View style={{ marginBottom: 16 }}>
-                        <Text style={{ fontSize: 14, marginBottom: 8, color: '#555' }}>Receiver’s contact</Text>
-                        <View style={{ flexDirection: 'row', alignItems: 'center', borderWidth: 1, borderColor: '#e0e0e0', borderRadius: 8, padding: 10, backgroundColor: '#fff' }}>
-                            <Text style={{ fontSize: 16, color: '#000', marginRight: 4 }}>+91</Text>
+                    <View style={{}}>
+                        <Text style={{ color: '#9297a0', fontWeight: '500', fontSize: responsiveFontSize(1.9), marginBottom: 8 }}>Receiver’s contact</Text>
+                        <View style={{ height: 40, width: '100%', flexDirection: 'row', alignItems: 'center', borderRadius: 8, borderColor: offWhite, borderWidth: 1.2, paddingHorizontal: 10, justifyContent: 'flex-start' }}>
+                            <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2), marginRight: 2, }}>+91</Text>
                             <TextInput
+                                style={{ fontWeight: "500", fontSize: responsiveFontSize(2), color: '#000', width: '80%' }}
+                                placeholder="Enter Contact No"
                                 value={contact}
                                 onChangeText={setContact}
-                                keyboardType="numeric"
-                                placeholder="Enter contact number"
-                                style={{ flex: 1, fontSize: 16, color: '#000' }}
+                                placeholderTextColor={'#c8cacf'}
                             />
-                            <TouchableOpacity onPress={() => setContact('')}>
-                                <Icon name="close" size={20} color="#999" />
-                            </TouchableOpacity>
                         </View>
-                        <Text style={{ fontSize: 12, color: '#999', marginTop: 4 }}>May be used to assist delivery</Text>
+                        <Text style={{ fontSize: responsiveFontSize(1.5), color: offWhite, marginTop: 2, textAlign: 'right' }}>*May be used to assist delivery</Text>
                     </View>
                 </View>
 
                 {/* Address Type */}
-                <View style={{ marginBottom: 16 }}>
-                    <Text style={{ fontSize: 14, marginBottom: 8, color: '#555' }}>Save address as *</Text>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        {['Home', 'Work', 'Hotel'].map((type) => (
+                <View style={{ marginTop: 10, paddingHorizontal: 13, paddingVertical: 12, backgroundColor: '#fff', elevation: 1, borderRadius: 12, }}>
+                    <Text style={{ color: '#9297a0', fontWeight: '500', fontSize: responsiveFontSize(1.9), marginBottom: 8 }}>Save address as *</Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+                        {['Home', 'Work'].map((type) => (
                             <TouchableOpacity
                                 key={type}
                                 style={{
-                                    flex: 1,
-                                    alignItems: 'center',
-                                    padding: 10,
+                                    width: 80,
+                                    height: 30,
                                     borderRadius: 8,
                                     borderWidth: 1,
-                                    borderColor: addressType === type ? '#5EC467' : '#e0e0e0',
-                                    backgroundColor: addressType === type ? '#FCECED' : '#fff',
-                                    marginRight: type !== 'Hotel' ? 8 : 0
+                                    borderColor: addressType === type ? backIconColor : '#e0e0e0',
+                                    backgroundColor: addressType === type ? lightGreen : '#fff',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    gap: 4
                                 }}
                                 onPress={() => setAddressType(type)}
                             >
-                                <Icon
-                                    name={type === 'Home' ? 'home' : type === 'Work' ? 'briefcase' : 'bed'}
-                                    size={20}
-                                    color={addressType === type ? '#CB202D' : '#555'}
+                                <Icon3
+                                    name={type === 'Home' ? 'home' : 'building'}
+                                    size={13}
+                                    color={addressType === type ? backIconColor : '#000'}
                                 />
-                                <Text style={{ fontSize: 14, color: '#555', marginTop: 4 }}>{type}</Text>
+                                <Text style={{ fontSize: responsiveFontSize(1.7), color: '#000', fontWeight: '600' }}>{type}</Text>
                             </TouchableOpacity>
                         ))}
-                        <TouchableOpacity style={{ flex: 0.15, alignItems: 'center', padding: 10, borderRadius: 8, borderWidth: 1, borderColor: '#e0e0e0', backgroundColor: '#fff' }}>
-                            <Icon name="map-marker" size={20} color="#555" />
-                        </TouchableOpacity>
                     </View>
                 </View>
 
                 {/* Address Details */}
-                <View style={{ marginBottom: 16 }}>
+                <View style={{ marginTop: 10 }}>
                     <Text style={{ fontSize: 16, color: '#555' }}>Christian Basti, Guwahati</Text>
                     <TouchableOpacity style={{ paddingVertical: 6, paddingHorizontal: 12, backgroundColor: '#F1F7FB', borderRadius: 4, alignSelf: 'flex-start', marginTop: 8 }}>
                         <Text style={{ fontSize: 14, color: '#CB202D' }}>Change</Text>
@@ -149,11 +144,19 @@ const AddNewAddress = () => {
                     />
                 </View>
 
-                {/* Confirm Address Button */}
-                <TouchableOpacity style={{ backgroundColor: '#DADFE2', padding: 16, borderRadius: 8, alignItems: 'center' }}>
-                    <Text style={{ fontSize: 16, color: '#fff', fontWeight: 'bold' }}>Confirm address</Text>
-                </TouchableOpacity>
             </View>
+
+            {/* Confirm Address Button */}
+            <LinearGradient
+                colors={[darkGreen, '#3a9f43']}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                style={{ borderRadius: 12, paddingVertical: 13, paddingHorizontal: 24, elevation: 2, marginTop: 30 }}
+            >
+                {/* <TouchableOpacity style={{ backgroundColor: '#DADFE2', padding: 16, borderRadius: 8, alignItems: 'center', position: 'absolute', bottom: 8, width: '96%', alignSelf: 'center' }}>
+                    <Text style={{ fontSize: 16, color: '#fff', fontWeight: 'bold' }}>Confirm address</Text>
+                </TouchableOpacity> */}
+            </LinearGradient>
 
         </SafeAreaView>
     )
