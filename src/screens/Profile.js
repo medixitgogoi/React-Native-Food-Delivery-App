9dd0c32ba38c07d8f98fbd4538cc98f70c229678
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-08-27 13:17:57 +0530

    updated

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,14 +3,16 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { background, backIconColor, darkGreen } from '../utils/colors';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../redux/LoginSlice';
-import { createEntityAdapter } from '@reduxjs/toolkit';
+import { deleteAllItemsFromCart } from '../redux/CartSlice';
 
 const Profile = () => {
 
     const dispatch = useDispatch();
 
-    const cartProducts = useSelector(state => state.cart);
-    console.log('cartProducts', cartProducts);
+    const logOutHandler = () => {
+        dispatch(deleteAllItemsFromCart());
+        dispatch(logout());
+    }
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: background, paddingBottom: 10 }}>
@@ -19,7 +21,7 @@ const Profile = () => {
                 backgroundColor={background}
                 barStyle="dark-content"
             />
-            <TouchableOpacity style={{ backgroundColor: backIconColor, alignSelf: 'center', height: 40, marginTop: 10, padding: 5, borderRadius: 10, width: '90%', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }} onPress={() => dispatch(logout())}>
+            <TouchableOpacity style={{ backgroundColor: backIconColor, alignSelf: 'center', height: 40, marginTop: 10, padding: 5, borderRadius: 10, width: '90%', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }} onPress={logOutHandler}>
                 <Text style={{ color: '#fff', fontWeight: '600' }}>Log Out</Text>
             </TouchableOpacity>
         </SafeAreaView>
