fef3474e3c7a58be5ed249450a7e543c54677b36
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-09-02 11:00:16 +0530

    updated

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,11 +3,14 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { background, backIconColor, darkGreen } from '../utils/colors';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../redux/LoginSlice';
+import Icon4 from 'react-native-vector-icons/dist/AntDesign';
 import { deleteAllItemsFromCart } from '../redux/CartSlice';
-import { useFocusEffect } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { useCallback } from 'react';
 
 const Profile = () => {
+
+    const navigation = useNavigation();
 
     const dispatch = useDispatch();
 
@@ -30,6 +33,12 @@ const Profile = () => {
                 backgroundColor={background}
                 barStyle="dark-content"
             />
+
+            {/* Header */}
+            <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingVertical: 10, paddingHorizontal: 13, alignSelf: 'flex-start' }}>
+                <Icon4 name="arrowleft" size={23} color={'#000'} />
+            </TouchableOpacity>
+
             <TouchableOpacity style={{ backgroundColor: backIconColor, alignSelf: 'center', height: 40, marginTop: 10, padding: 5, borderRadius: 10, width: '90%', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }} onPress={logOutHandler}>
                 <Text style={{ color: '#fff', fontWeight: '600' }}>Log Out</Text>
             </TouchableOpacity>
