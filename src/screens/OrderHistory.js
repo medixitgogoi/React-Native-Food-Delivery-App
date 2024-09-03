5f568de244e7462ef836a6a30deed2458b97fb5a
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-09-03 17:55:05 +0530

    updated

--- src/screens/OrderHistory.js
+++ src/screens/OrderHistory.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, StatusBar, TextInput, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, StatusBar, TextInput, ScrollView, Dimensions, FlatList } from 'react-native';
 import { responsiveFontSize } from 'react-native-responsive-dimensions';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { background, backIconColor } from '../utils/colors';
@@ -6,7 +6,10 @@ import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 import Icon4 from 'react-native-vector-icons/dist/AntDesign';
 import { useNavigation } from '@react-navigation/native';
 import Icon5 from 'react-native-vector-icons/dist/Ionicons';
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
+import { orders } from '../utils/orders';
+
+const { width: screenWidth } = Dimensions.get('window');
 
 const OrderHistory = () => {
 
@@ -15,24 +18,81 @@ const OrderHistory = () => {
     const [search, setSearch] = useState('');
     const [isSearchFocused, setIsSearchFocused] = useState(false);
 
-    const orders = [
-        {
-            id: 1,
-            restaurantName: 'Pizza Palace',
-            orderDate: '2024-09-01',
-            status: 'Delivered',
-            items: ['Pepperoni Pizza', 'Garlic Bread'],
-            total: '$25.00',
-        },
-        {
-            id: 2,
-            restaurantName: 'Burger Hub',
-            orderDate: '2024-08-30',
-            status: 'On the Way',
-            items: ['Cheese Burger', 'Fries', 'Coke'],
-            total: '$18.50',
-        },
-    ];
+    const [loading, setLoading] = useState(false);
+
+    const [filteredNames, setFilteredNames] = useState([]);
+
+    const debouncedSearch = useMemo(() => debounce((text) => {
+        setFilteredNames(orders.filter(order => order.name.toLowerCase().includes(text.toLowerCase())));
+    }, 300), [data]);
+
+    const handleSearch = (text) => {
+        setSearch(text);
+        debouncedSearch(text);
+    };
+
+    const renderOrder = useCallback(({ item }) => (
+        <OrderItem item={item} search={search} />
+    ), [search]);
+
+    const OrderItem = ({ item, search }) => {
+
+        // search text
+        // const getHighlightedText = (text, highlight) => {
+        //     const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+        //     return (
+        //         <Text>
+        //             {parts.map((part, index) =>
+        //                 part.toLowerCase() === highlight.toLowerCase() ? (
+        //                     <Text key={index} style={{ backgroundColor: 'yellow' }}>{part}</Text>
+        //                 ) : (
+        //                     <Text key={index}>{part}</Text>
+        //                 )
+        //             )}
+        //         </Text>
+        //     );
+        // };
+
+        return (
+            <TouchableOpacity onPress={() => navigation.navigate('ProductDetails', { data: item })} key={item?.id} style={{ width: screenWidth / 2.2, marginVertical: 6, backgroundColor: '#fff', borderTopLeftRadius: 14, borderTopRightRadius: 14, borderBottomLeftRadius: 14, borderBottomRightRadius: 20, overflow: 'hidden', elevation: 2, }}>
+
+                <TouchableOpacity style={{ zIndex: 10, backgroundColor: '#c6e6c3', borderRadius: 50, position: 'absolute', top: 8, right: 8, width: 30, height: 30, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                    <Icon name="favorite-border" size={18} color={'#019934'} />
+                </TouchableOpacity>
+
+                <View style={{ backgroundColor: lightGreen, borderRadius: 12, margin: 3 }}>
+                    <View style={{ padding: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                        <Image source={require('../assets/orange.png')} style={{ width: '100%', height: 100, resizeMode: 'contain' }} />
+                    </View>
+                </View>
+
+                <View style={{ padding: 10 }}>
+                    <View style={{ flexDirection: 'column', gap: 3 }}>
+                        <Text style={{ fontSize: responsiveFontSize(2), fontWeight: '600', color: '#000' }} numberOfLines={1} ellipsizeMode='tail'>{getHighlightedText(item.name, search)}</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 4 }}>
+                            <StarRating rating={item.starRating} />
+                            <View style={{ backgroundColor: backIconColor, paddingVertical: 2, paddingHorizontal: 4, gap: 2, borderRadius: 5, flexDirection: 'row', alignItems: 'center' }}>
+                                <Text style={{ color: '#fff', fontSize: responsiveFontSize(1.5), fontWeight: '500' }}>{item.starRating}</Text>
+                                <Icon3 name="star" size={10} color={'#fff'} style={{ margin: 0, padding: 0, alignSelf: 'center' }} />
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: 'row', marginBottom: 5, marginTop: 2 }}>
+                        <Text style={{ color: offWhite, fontWeight: '600', fontSize: responsiveFontSize(1.8) }}>{item.subCategory}</Text>
+                    </View>
+                    <View style={{ flexDirection: 'row', alignItems: 'flex-end', gap: 3 }}>
+                        <Text style={{ fontSize: responsiveFontSize(2.3), color: '#019934', fontWeight: '800' }}>₹{item.units[0].discountedPrice}</Text>
+                        <Text style={{ fontSize: responsiveFontSize(1.5), color: offWhite, fontWeight: '600', paddingBottom: 2, textDecorationLine: 'line-through' }}>₹{item.units[0].price}</Text>
+                    </View>
+                </View>
+
+                {/* <TouchableOpacity style={{ backgroundColor: '#019934', borderTopLeftRadius: 10, width: 35, height: 35, justifyContent: 'center', alignItems: 'center', position: 'absolute', bottom: 0, right: 0 }}>
+                    <Icon name="add" size={20} color="#fff" />
+                </TouchableOpacity> */}
+
+            </TouchableOpacity>
+        );
+    };
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: background, paddingBottom: 60 }}>
@@ -69,11 +129,23 @@ const OrderHistory = () => {
                 </View>
             </View>
 
-            <ScrollView style={{ flex: 1 }}>
-                <View style={{ backgroundColor: '#fff' }}>
-
-                </View>
-            </ScrollView>
+            {/* Content */}
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flexWrap: 'wrap', width: screenWidth }}>
+                {loading ? (
+                    <View>
+                        <Text style={{ color: '#000', textAlign: 'center' }}>Loading ...</Text>
+                    </View>
+                ) : (
+                    <FlatList
+                        data={orders}
+                        renderItem={renderOrder}
+                        keyExtractor={item => item.id.toString()}
+                        showsVerticalScrollIndicator={false}
+                        contentContainerStyle={{ paddingHorizontal: 10, paddingBottom: 90, paddingTop: 4 }}
+                        // columnWrapperStyle={{ justifyContent: 'space-between' }}
+                    />
+                )}
+            </View>
 
         </SafeAreaView>
     )
---
+++ src/utils/orders.js
@@ -0,0 +1,182 @@
+export const orders = [
+    {
+        id: 1,
+        orders: [
+            {
+                id: 1,
+                name: 'Cheese',
+                description: 'Experience the richness of creamy cheddar cheese, a versatile delight that elevates every dish it touches. Whether melted in sandwiches, sprinkled over salads, or enjoyed on its own, this cheese offers a perfect balance of flavor and texture, making it a must-have for cheese lovers and culinary enthusiasts alike.',
+                image: '../assets/grocery.jpeg',
+                subCategory: 'Fruit',
+                starRating: 4,
+                type: 'grocery',
+                quantity: 4,
+                units: {
+                    id: 1,
+                    unit: '250 gm',
+                    price: 120,
+                    discountedPrice: 100,
+                }
+            },
+            {
+                id: 2,
+                name: 'Gobi Manchurian',
+                description: 'Crispy cauliflower florets tossed in a tangy and spicy sauce, offering a burst of flavors with every bite. This popular Indo-Chinese dish is loved for its crunchy texture and bold taste.',
+                image: '../assets/gobi-manchurian.png',
+                subCategory: 'Veg',
+                starRating: 4.5,
+                type: 'restaurant',
+                quantity: 2,
+                units: {
+                    id: 1,
+                    unit: 'Half Plate',
+                    price: 169,
+                    discountedPrice: 149,
+                }
+            },
+            {
+                id: 3,
+                name: 'Tiramisu Cheesecake',
+                description: 'Indulge in the rich and creamy flavors of our Tiramisu Cheesecake, combining layers of classic tiramisu with a creamy cheesecake base. This dessert offers a luxurious blend of coffee, mascarpone, and chocolate.',
+                image: '../assets/cake.jpeg',
+                subCategory: 'Non-veg',
+                starRating: 4.7,
+                quantity: 2,
+                type: 'cake',
+                units: {
+                    id: 1,
+                    unit: '1/2 Kg',
+                    price: 399,
+                    discountedPrice: 369,
+                }
+            },
+        ]
+    },
+    {
+        id: 2,
+        orders: [
+            {
+                id: 1,
+                name: 'Tandoori Chicken',
+                description: 'A classic Indian dish featuring marinated chicken roasted in a tandoor oven, resulting in a smoky and flavorful experience. Served with lemon and mint chutney, this dish is a hit among non-veg lovers.',
+                image: '../assets/tandoori-chicken.png',
+                subCategory: 'Non-Veg',
+                starRating: 4.8,
+                type: 'restaurant',
+                quantity: 6,
+                units: {
+                    id: 1,
+                    unit: 'Half Plate',
+                    price: 329,
+                    discountedPrice: 299,
+                }
+            },
+            {
+                id: 2,
+                name: 'Gobi Manchurian',
+                description: 'Crispy cauliflower florets tossed in a tangy and spicy sauce, offering a burst of flavors with every bite. This popular Indo-Chinese dish is loved for its crunchy texture and bold taste.',
+                image: '../assets/gobi-manchurian.png',
+                subCategory: 'Veg',
+                starRating: 4.5,
+                type: 'restaurant',
+                quantity: 2,
+                units: {
+                    id: 1,
+                    unit: 'Half Plate',
+                    price: 169,
+                    discountedPrice: 149,
+                }
+            },
+        ]
+    },
+    {
+        id: 3,
+        orders: [
+            {
+                id: 1,
+                name: 'Tandoori Chicken',
+                description: 'A classic Indian dish featuring marinated chicken roasted in a tandoor oven, resulting in a smoky and flavorful experience. Served with lemon and mint chutney, this dish is a hit among non-veg lovers.',
+                image: '../assets/tandoori-chicken.png',
+                subCategory: 'Non-Veg',
+                starRating: 4.8,
+                type: 'restaurant',
+                quantity: 6,
+                units: {
+                    id: 1,
+                    unit: 'Half Plate',
+                    price: 329,
+                    discountedPrice: 299,
+                }
+            },
+            {
+                id: 2,
+                name: 'Honey',
+                description: 'Pure and natural honey, rich in flavor and packed with health benefits. Ideal for sweetening tea, drizzling over yogurt, or using in baking. This honey is sourced from the finest beekeepers for optimal quality.',
+                image: '../assets/grocery_honey.jpeg',
+                subCategory: 'Sweeteners',
+                starRating: 5,
+                type: 'grocery',
+                quantity: 4,
+                units: {
+                    id: 1,
+                    unit: '250 gm',
+                    price: 150,
+                    discountedPrice: 140,
+                }
+            },
+        ]
+    },
+    {
+        id: 4,
+        orders: [
+            {
+                id: 1,
+                name: 'Tandoori Chicken',
+                description: 'A classic Indian dish featuring marinated chicken roasted in a tandoor oven, resulting in a smoky and flavorful experience. Served with lemon and mint chutney, this dish is a hit among non-veg lovers.',
+                image: '../assets/tandoori-chicken.png',
+                subCategory: 'Non-Veg',
+                starRating: 4.8,
+                type: 'restaurant',
+                quantity: 6,
+                units: {
+                    id: 1,
+                    unit: 'Half Plate',
+                    price: 329,
+                    discountedPrice: 299,
+                }
+            },
+            {
+                id: 2,
+                name: 'Gobi Manchurian',
+                description: 'Crispy cauliflower florets tossed in a tangy and spicy sauce, offering a burst of flavors with every bite. This popular Indo-Chinese dish is loved for its crunchy texture and bold taste.',
+                image: '../assets/gobi-manchurian.png',
+                subCategory: 'Veg',
+                starRating: 4.5,
+                type: 'restaurant',
+                quantity: 2,
+                units: {
+                    id: 1,
+                    unit: 'Half Plate',
+                    price: 169,
+                    discountedPrice: 149,
+                }
+            },
+            {
+                id: 3,
+                name: 'Orange Almond Cake',
+                description: 'Enjoy the nutty and citrus flavors of our Orange Almond Cake, with a moist sponge infused with orange zest and chopped almonds. This cake offers a unique blend of sweet and tangy with a satisfying crunch.',
+                image: '../assets/cake.jpeg',
+                subCategory: 'Veg',
+                starRating: 4.5,
+                type: 'cake',
+                quantity: 1,
+                units: {
+                    id: 1,
+                    unit: '1/2 Kg',
+                    price: 349,
+                    discountedPrice: 319,
+                },
+            },
+        ]
+    },
+]
