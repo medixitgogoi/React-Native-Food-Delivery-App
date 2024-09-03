207dd2130ae30e6f278a5a1c3fccce82f79ad12c
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-09-03 15:52:26 +0530

    updated

--- src/screens/OrderHistory.js
+++ src/screens/OrderHistory.js
@@ -1,14 +1,19 @@
-import { View, Text, TouchableOpacity, StatusBar } from 'react-native';
+import { View, Text, TouchableOpacity, StatusBar, TextInput } from 'react-native';
 import { responsiveFontSize } from 'react-native-responsive-dimensions';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { background } from '../utils/colors';
+import { background, backIconColor } from '../utils/colors';
 import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 import Icon4 from 'react-native-vector-icons/dist/AntDesign';
 import { useNavigation } from '@react-navigation/native';
+import Icon5 from 'react-native-vector-icons/dist/Ionicons';
+import { useState } from 'react';
 
 const OrderHistory = () => {
 
     const navigation = useNavigation();
+
+    const [search, setSearch] = useState('');
+    const [isSearchFocused, setIsSearchFocused] = useState(false);
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: background, paddingBottom: 60 }}>
@@ -19,14 +24,36 @@ const OrderHistory = () => {
             />
 
             {/* Header */}
-            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8, paddingVertical: 5 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5, paddingVertical: 8 }}>
                 <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingVertical: 5, paddingHorizontal: 10, alignSelf: 'flex-start' }}>
                     <Icon4 name="arrowleft" size={23} color={'#000'} />
                 </TouchableOpacity>
                 <Text style={{ color: '#000', fontSize: responsiveFontSize(2.4), fontWeight: '500' }}>Your Orders</Text>
             </View>
 
-            <Text style={{ color: '#000' }}>OrderHistory</Text>
+            {/* searchbar */}
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 12, marginVertical: 8 }}>
+                <View style={{ width: '100%', borderColor: isSearchFocused ? backIconColor : '#F9FAFD', borderWidth: 1, flexDirection: 'row', alignItems: 'center', backgroundColor: '#fff', borderRadius: 11, paddingHorizontal: 8, elevation: 2 }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: 40, width: 23, }}>
+                        <Icon5 name="search" size={20} color={backIconColor} style={{ margin: 0, padding: 0 }} />
+                    </View>
+                    <TextInput
+                        style={{ paddingVertical: 0, height: 40, color: '#000', fontWeight: '500', letterSpacing: 0.3, width: '87%', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', }}
+                        placeholder="Search by dish, cake or grocery"
+                        placeholderTextColor="#a0abb7"
+                        // onChangeText={handleSearch}
+                        onChangeText={setSearch}
+                        value={search}
+                        onFocus={() => setIsSearchFocused(true)}
+                        onBlur={() => setIsSearchFocused(false)}
+                    />
+                </View>
+            </View>
+
+            <View>
+
+            </View>
+
         </SafeAreaView>
     )
 }
