56a0733d4b38dc4e28d852403b8af4df8ea72efa
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-09-03 13:29:24 +0530

    updated

--- src/screens/OrderHistory.js
+++ src/screens/OrderHistory.js
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StatusBar } from 'react-native';
+import { responsiveFontSize } from 'react-native-responsive-dimensions';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { background } from '../utils/colors';
+import Icon from 'react-native-vector-icons/dist/MaterialIcons';
+import Icon4 from 'react-native-vector-icons/dist/AntDesign';
+import { useNavigation } from '@react-navigation/native';
 
 const OrderHistory = () => {
+
+    const navigation = useNavigation();
+
     return (
-        <View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: background, paddingBottom: 60 }}>
+            <StatusBar
+                animated={true}
+                backgroundColor={background}
+                barStyle="dark-content"
+            />
+
+            {/* Header */}
+            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8, paddingVertical: 5 }}>
+                <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingVertical: 5, paddingHorizontal: 10, alignSelf: 'flex-start' }}>
+                    <Icon4 name="arrowleft" size={23} color={'#000'} />
+                </TouchableOpacity>
+                <Text style={{ color: '#000', fontSize: responsiveFontSize(2.4), fontWeight: '500' }}>Your Orders</Text>
+            </View>
+
             <Text style={{ color: '#000' }}>OrderHistory</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
