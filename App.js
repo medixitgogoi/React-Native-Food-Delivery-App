838c285e23c1c6540549b6c914752e6374e26564
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-09-18 12:52:31 +0530

    updated

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import { StyleSheet, PermissionsAndroid, Platform } from 'react-native';
+import { StyleSheet, PermissionsAndroid, Platform, Text } from 'react-native';
 import StackNavigation from './src/navigation/StackNavigation';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/Store';
 import { useEffect } from 'react';
+import Toast from 'react-native-toast-message';
+import { responsiveFontSize } from 'react-native-responsive-dimensions';
+import { backIconColor, lightGreen } from './src/utils/colors';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
 
@@ -34,11 +38,46 @@ const App = () => {
 
     useEffect(() => {
         requestLocationPermission();
-    })
+    });
+
+    const toastConfig = {
+        customToast: ({ text1, text2 }) => (
+            <SafeAreaView
+                style={{
+                    height: 60,
+                    width: '95%',
+                    padding: 10,
+                    backgroundColor: lightGreen, // Custom background color
+                    alignSelf: 'center',
+                    borderLeftColor: '#c23817',      // Red left border
+                    borderLeftWidth: 5,          // Thicker left border
+                    borderTopColor: '#000',      // Black border on top
+                    borderTopWidth: 1,
+                    borderRightColor: '#000',    // Black border on right
+                    borderRightWidth: 1,
+                    borderBottomColor: '#000',   // Black border on bottom
+                    borderBottomWidth: 1,
+                    justifyContent: 'center',    // Center content vertically
+                    borderRadius: 10,
+                    flexDirection: 'column',
+                    gap: 3
+                }}
+            >
+                <Text style={{ fontSize: responsiveFontSize(1.9), fontWeight: 'bold', color: backIconColor }}>
+                    {text1}
+                </Text>
+                <Text style={{ fontSize: responsiveFontSize(1.6), color: '#000' }}>
+                    {text2}
+                </Text>
+            </SafeAreaView>
+        ),
+    };
+
 
     return (
         <Provider store={store}>
             <StackNavigation />
+            <Toast config={toastConfig} />
         </Provider>
     )
 }
