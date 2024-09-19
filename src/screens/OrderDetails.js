5bbaded3296a76d0824d4251fa1a46ddcb8a89cb
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-09-19 15:12:04 +0530

    updated

--- src/screens/OrderDetails.js
+++ src/screens/OrderDetails.js
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native';
 
-const OrderDetails = () => {
-  return (
-    <View>
-      <Text>OrderDetails</Text>
-    </View>
-  )
+const OrderDetails = ({ route }) => {
+
+    console.log('route', route?.params?.detail);
+
+    return (
+        <View>
+            <Text>OrderDetails</Text>
+        </View>
+    )
 }
 
 export default OrderDetails;
