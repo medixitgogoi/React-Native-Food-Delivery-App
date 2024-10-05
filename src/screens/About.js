cbb99b2bc7df1b5c04f85e5d367c90e37a8a641c
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-10-05 14:27:38 +0530

    updated

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,15 +1,52 @@
-import { View, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, ScrollView, StatusBar, TouchableOpacity } from 'react-native';
+import { responsiveFontSize } from 'react-native-responsive-dimensions';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon4 from 'react-native-vector-icons/dist/AntDesign';
 
 const About = () => {
 
-    const wishlistItems = useSelector(state => state.wishlist.items);
-    console.log('wishlistItems', wishlistItems);
+    const navigation = useNavigation();
 
     return (
-        <View>
-            <Text style={{ color: '#000' }}>About</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+            <StatusBar
+                animated={true}
+                backgroundColor={'#fff'}
+                barStyle="dark-content"
+            />
+
+            {/* Header */}
+            <View style={{ flexDirection: 'row', alignItems: 'center', paddingVertical: 8 }}>
+                <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingVertical: 5, paddingHorizontal: 10 }}>
+                    <Icon4 name="arrowleft" size={22} color={'#000'} />
+                </TouchableOpacity>
+                <View style={{ position: 'absolute', left: 0, right: 0, flexDirection: 'row', justifyContent: 'center', }}>
+                    <Text style={{ color: '#000', fontSize: responsiveFontSize(2.4), fontWeight: '500' }}>About</Text>
+                </View>
+            </View>
+
+            {/* Content */}
+            <ScrollView>
+                <View style={{ flexDirection: 'column', paddingHorizontal: 12, marginTop: 10 }}>
+                    <Text style={{ color: '#000', textAlign: 'justify' }}>
+                        Welcome to Skercart, your go-to app for fast and convenient grocery delivery. We aim to provide a seamless shopping experience, offering a wide range of products from fresh produce to everyday essentials, all delivered right to your doorstep.
+
+                        Please note that product availability can vary due to stock levels and supply chain factors. While we do our best to keep inventory up-to-date, there may be rare occasions when certain items become unavailable after you've placed an order. In such cases, we will notify you promptly, offering alternatives or refunds as needed.
+
+                        Skercart strives to provide accurate product details, pricing, and nutritional information. However, product images and descriptions may differ slightly from the actual items delivered. We encourage you to check product labels directly for the most reliable information, especially if you have dietary restrictions or allergies.
+
+                        Our team works hard to deliver your orders on time, but delivery windows are estimates and can be affected by factors like traffic, weather, or operational issues. Should any delays occur, we will keep you informed and work to resolve the situation as quickly as possible.
+
+                        Skercart partners with trusted third-party vendors and delivery providers to ensure a smooth operation. We are committed to quality service, but we cannot take responsibility for actions or errors by third-party suppliers. Any concerns related to delivery or payment should be addressed to the respective provider.
+
+                        Your health and safety are our priority. We adhere to strict food safety guidelines, and we encourage you to inspect your orders upon delivery. By using Skercart, you acknowledge and accept our terms of service, and we advise reviewing these periodically as we may update them from time to time.
+
+                        Thank you for choosing Skercart for your grocery needs. We look forward to serving you!
+                    </Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
--- src/screens/FAQ.js
+++ src/screens/FAQ.js
@@ -1,11 +1,106 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, ScrollView, StatusBar, TouchableOpacity } from 'react-native';
+import { responsiveFontSize } from 'react-native-responsive-dimensions';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon4 from 'react-native-vector-icons/dist/AntDesign';
 
 const FAQ = () => {
+
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text style={{ color: '#000' }}>FAQ</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+            <StatusBar
+                animated={true}
+                backgroundColor={'#fff'}
+                barStyle="dark-content"
+            />
+
+            {/* Header */}
+            <View style={{ flexDirection: 'row', alignItems: 'center', paddingVertical: 8 }}>
+                <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingVertical: 5, paddingHorizontal: 10 }}>
+                    <Icon4 name="arrowleft" size={22} color={'#000'} />
+                </TouchableOpacity>
+                <View style={{ position: 'absolute', left: 0, right: 0, flexDirection: 'row', justifyContent: 'center', }}>
+                    <Text style={{ color: '#000', fontSize: responsiveFontSize(2.4), fontWeight: '500' }}>FAQ</Text>
+                </View>
+            </View>
+
+            {/* Content */}
+            <ScrollView>
+                <View style={{ flexDirection: 'column', paddingHorizontal: 12, paddingBottom: 20 }}>
+                    <View style={{ marginVertical: 8 }}>
+                        <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.1) }}>1. What products and services does Skercart offer?</Text>
+                        <Text style={{ color: '#000', marginTop: 3, fontSize: responsiveFontSize(1.9) }}>
+                            Skercart provides a wide selection of groceries, household essentials, and more. We strive to offer the freshest products, but availability may change due to stock levels, demand, and supply constraints.
+                        </Text>
+                    </View>
+
+                    <View style={{ marginVertical: 5 }}>
+                        <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.1) }}>2. Can I be sure all items I order will be available?</Text>
+                        <Text style={{ color: '#000', marginTop: 3, fontSize: responsiveFontSize(1.9) }}>
+                            While we do our best to keep all products in stock, there are times when items may be unavailable. In such cases, we'll notify you and offer a substitution or refund.
+                        </Text>
+                    </View>
+
+                    <View style={{ marginVertical: 5 }}>
+                        <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.1) }}>3. How are prices determined on Skercart?</Text>
+                        <Text style={{ color: '#000', marginTop: 3, fontSize: responsiveFontSize(1.9) }}>
+                            Prices are updated regularly to reflect market conditions and promotions. Please note that prices may change without notice. The final cost, including any taxes and delivery fees, will be confirmed at checkout.
+                        </Text>
+                    </View>
+
+                    <View style={{ marginVertical: 5 }}>
+                        <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.1) }}>4. Is Skercart the cheapest option for groceries?</Text>
+                        <Text style={{ color: '#000', marginTop: 3, fontSize: responsiveFontSize(1.9) }}>
+                            We do not guarantee the lowest prices, but we work hard to provide competitive pricing on all products.
+                        </Text>
+                    </View>
+
+                    <View style={{ marginVertical: 5 }}>
+                        <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.1) }}>5. What are the delivery times, and are they guaranteed?</Text>
+                        <Text style={{ color: '#000', marginTop: 3, fontSize: responsiveFontSize(1.9) }}>
+                            We aim to deliver within the estimated timeframes shown during checkout. However, delivery times are not guaranteed and may be impacted by external factors such as weather, traffic, or operational delays. We’ll do our best to keep you informed if any delays occur.
+                        </Text>
+                    </View>
+
+                    <View style={{ marginVertical: 5 }}>
+                        <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.1) }}>6. What happens if the product images or descriptions don’t match the actual product?</Text>
+                        <Text style={{ color: '#000', marginTop: 3, fontSize: responsiveFontSize(1.9) }}>
+                            We strive to provide accurate images and descriptions of all products. However, variations may occur. We encourage customers to review product labels and descriptions, especially for important details such as allergens or nutritional information.
+                        </Text>
+                    </View>
+
+                    <View style={{ marginVertical: 5 }}>
+                        <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.1) }}>7. Can I return an item or request a refund?</Text>
+                        <Text style={{ color: '#000', marginTop: 3, fontSize: responsiveFontSize(1.9) }}>
+                            If an item is damaged or incorrect, please reach out to our customer support team. Refunds or replacements will be processed at our discretion.
+                        </Text>
+                    </View>
+
+                    <View style={{ marginVertical: 5 }}>
+                        <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.1) }}>8. What about food safety?</Text>
+                        <Text style={{ color: '#000', marginTop: 3, fontSize: responsiveFontSize(1.9) }}>
+                            We are committed to maintaining high food safety standards. Please inspect your order upon delivery to ensure everything is in good condition. Follow appropriate food storage guidelines after receiving your groceries.
+                        </Text>
+                    </View>
+
+                    <View style={{ marginVertical: 5 }}>
+                        <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.1) }}>9. Do you work with third-party suppliers and services?</Text>
+                        <Text style={{ color: '#000', marginTop: 3, fontSize: responsiveFontSize(1.9) }}>
+                            Yes, we partner with third-party suppliers, delivery services, and payment processors. While we work with trusted partners, we are not responsible for their actions or any issues that may arise. Please contact them directly for any concerns.
+                        </Text>
+                    </View>
+
+                    <View style={{ marginVertical: 5 }}>
+                        <Text style={{ color: '#000', fontWeight: '600', fontSize: responsiveFontSize(2.1) }}>10. Are there any limitations to your service?</Text>
+                        <Text style={{ color: '#000', marginTop: 3, fontSize: responsiveFontSize(1.9) }}>
+                            To the fullest extent permitted by law, Skercart disclaims all warranties regarding the accuracy, quality, or reliability of the service. We are not liable for any direct or indirect damages that may arise from using the app.
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
