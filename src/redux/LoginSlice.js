7669ce9b0fccaacaa62f5f4dbe00fbae68fb2ff4
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-10-21 17:07:32 +0530

    updated

--- src/redux/LoginSlice.js
+++ src/redux/LoginSlice.js
@@ -19,38 +19,3 @@ export const loginSlice = createSlice({
 
 export const { login, logout } = loginSlice.actions;
 export default loginSlice.reducer;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const initialState = [];
-
-// export const loginSlice = createSlice({
-//     name: 'login',
-//     initialState,
-//     reducers: {
-//         addLoginUser: (state, action) => {
-//             state.push(action.payload);
-//         },
-//         logoutUser: (state) => {
-//             return state = [];
-//         },
-//     },
-// })
-
-// export const { addLoginUser, logoutUser } = loginSlice.actions;
-// export default loginSlice.reducer;
--- src/redux/Store.js
+++ src/redux/Store.js
@@ -2,17 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from "./CartSlice";
 import userReducer from "./UserSlice";
 import wishlistReducer from "./WishlistSlice";
-import themeReducer from './ThemeSlice';
 import loginReducer from "./LoginSlice";
-import writeUpReducer from './WriteUpSlice';
 
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
         user: userReducer,
         wishlist: wishlistReducer,
-        theme: themeReducer,
         login: loginReducer,
-        writeUp: writeUpReducer
     },
 })
